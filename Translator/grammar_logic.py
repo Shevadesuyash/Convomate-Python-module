0b9c30a20231b566391bd7f9706b1ecbc783bbc8
suyash <shevadesuyash@gmail.com>
2025-03-16 19:23:37 +0530

    xyz

--- Translator/grammar_logic.py
+++ Translator/grammar_logic.py
@@ -4,10 +4,14 @@ from transformers import T5ForConditionalGeneration, T5Tokenizer
 # Load LanguageTool for grammar and spelling correction
 tool = language_tool_python.LanguageTool('en-US')
 
+
+
 # Load T5 model for tense transformation
 model_name = "t5-small"
 tokenizer = T5Tokenizer.from_pretrained(model_name)
 model = T5ForConditionalGeneration.from_pretrained(model_name)
+
+
 
 # Function to correct grammar and spelling
 def correct_grammar(text):
